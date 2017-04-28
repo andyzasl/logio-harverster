56215680c337c260f4dbfcc478cded0899476247
Andrey Zaslavskiy <a.zaslavskiy@supportit.ru>
2017-04-28 10:39:11 +0300

    initial

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+server = dict(
+        host='172.16.253.52',
+        port=28777
+)
+log = dict(
+        path='/var/log/system.log',
+        streamid='system_log'
+)
+node = dict(
+        name='SIT-TESTNODE(1.1.2.1)'
+)
---
+++ havest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import socket
+
+import tailer
+
+import config
+
+server_addr = config.server['host']
+server_port = config.server['port']
+logfile = config.log['path']
+myname = config.node['name']
+streamid = config.log['streamid']
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((server_addr, server_port))
+print "Going to send data to " + server_addr + ":" + str(server_port)
+
+s.send('+node|' + str(myname) + '|' + str(streamid) + '\r\n')
+
+for line in tailer.follow(open(logfile)):
+    data = '+log|' + str(streamid) + '|' + str(myname) + '|info' + '|' + line + '\r\n'
+    s.send(data)
+
+# unregister me
+# s.send('-node|'+str(myname)+'\r\n')
+
+s.close()
